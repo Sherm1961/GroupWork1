f3174b4611542cd58f5b30609ed03826b8b71fbc
sherm <sherm@SHERMS_PC>
2025-09-09 21:24:21 -0400

    Add project files.

---
+++ GroupWork1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GroupWork1
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string sentence = Console.ReadLine();
+
+            int letterCount = 0;
+
+            foreach (char c in sentence)
+            {
+                if (char.IsLetter(c)) // checks if character is a letter (A-Z or a-z)
+                {
+                    letterCount++;
+                }
+            }
+
+            Console.WriteLine($"The number of letters in the sentence is: {letterCount}");
+        }
+    }
+}
